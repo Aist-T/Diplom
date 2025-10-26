7e8f6865815baa98c8abebde18409f9dcfefb1b2
Nasty <aistdac@mail.ru>
2025-10-26 16:39:46 +0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,4 @@
+UI_url = "https://www.chitai-gorod.ru"
+API1_url = "https://web-gate.chitai-gorod.ru/api/v1/cart/product"
+API2_url = "https://web-gate.chitai-gorod.ru/api/v1/cart"
+bearer_token = "Bearer%20eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjEwMzIwMDM5LCJpYXQiOjE3NjEzMDM2NzMsImV4cCI6MTc2MTMwNzI3MywidHlwZSI6MjAsImp0aSI6IjAxOWExNWUxLTNjODYtN2Y2Zi1hMDEwLTE2ZjFkMzM1NGNkMCIsInJvbGVzIjoxMH0.mufv40-0E3mDRf0mC95jVV60U0kPb02RN3bOlP3Ogjk"
